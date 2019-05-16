e9a4dd9a8f7b791dc3d9b935e5bcb428028ab63e
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-05-16 10:03:24 +0200

    frame-alloc-alg: init

---
+++ semester-2/operating-systems/frame-allocation-algorithms/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    int processesNum = 10,
+        maxPage = 100,
+        memorySize = 10,
+        maxRequestsSeqLen = 1000;
+
+
+    public static void main(String[] args) {
+        
+    }
+}
