19549d472d8bdd58bfbfc2ef9735c3d559b4f029
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-05-27 20:37:52 +0200

    frame-alloc-alg: optimized random ints generation

--- semester-2/operating-systems/frame-allocation-algorithms/src/utils/RandomNums.java
+++ semester-2/operating-systems/frame-allocation-algorithms/src/utils/RandomNums.java
@@ -4,24 +4,25 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomNums {
+    private static Random rand = new Random();
+
     public static int randomInt(int min, int max) {
         if (min > max) {
             throw new IllegalArgumentException("max must be greater than min");
         }
 
-        Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
     }
 
-    public static ArrayList<Integer> getSequnce(int seqLen, int min, int max) {
+    public static ArrayList<Integer> getSequnce(int seqLen, int minNum, int maxNum) {
         ArrayList<Integer> result = new ArrayList<>();
 
-        int localityRegions = max;
+        int localityRegions = maxNum;
         int numsPerRegion = seqLen / localityRegions;
         for (int region = 0; region < localityRegions; region++) {
             for (int i = 0; i < numsPerRegion; i++) {
-                int from = Math.min(min + region, min + (max - min) / 2);
-                int to = Math.min(min + numsPerRegion + region, max);
+                int from = Math.min(minNum + region, minNum + (maxNum - minNum) / 2);
+                int to = Math.min(minNum + numsPerRegion + region, maxNum);
                 int num = randomInt(from ,to);
                 result.add(num);
             }
