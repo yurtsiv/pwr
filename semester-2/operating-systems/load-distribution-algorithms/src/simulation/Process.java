4be512c2d6cf978a020fc6b6ce2463cd7cf7aaea
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-06-08 18:48:53 +0200

    load-distribution-algorithms: basic project structure

---
+++ semester-2/operating-systems/load-distribution-algorithms/src/simulation/Process.java
@@ -0,0 +1,22 @@
+package simulation;
+
+public class Process {
+    private int load, remainingTime;
+
+    public Process(int load, int remainingTime) {
+        this.load = load;
+        this.remainingTime = remainingTime;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public int getRemainingTime() {
+        return remainingTime;
+    }
+
+    public void decrementRemainingTime() {
+        remainingTime--;
+    }
+}
