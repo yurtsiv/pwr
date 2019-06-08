305fd20e501b381bbd71a13f0c8aa4b5c82e0d6b
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-06-08 19:57:52 +0200

    load-distribution-algorithms: Main

--- semester-2/operating-systems/load-distribution-algorithms/src/Main.java
+++ semester-2/operating-systems/load-distribution-algorithms/src/Main.java
@@ -1,5 +1,24 @@
+import algorithms.Algorithm1;
+import simulation.*;
+import simulation.Process;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
+    static int
+        p = 3,
+        r = 10,
+        z = 10,
+        n = 50,
+        processNum = 1000;
+
     public static void main(String[] args) {
-        System.out.println();
+        SimulationConfig config = new SimulationConfig(p, r, z, n);
+
+        Simulation simulation = new Simulation();
+        ArrayList<Process> processes = ProcessGenerator.generate(processNum);
+
+        ArrayList<Processor> alg1Result = simulation.run(processes, config, new Algorithm1());
     }
 }
