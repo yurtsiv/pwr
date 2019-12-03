f7fafc0677477399cdf414d47bfa2ef341c510c5
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2019-12-03 22:38:49 +0100

    tep lab 7 completed

--- semester-3/tep/lab7/CMySmartPointer.h
+++ semester-3/tep/lab7/CMySmartPointer.h
@@ -1,9 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
 class CRefCounter
 {
     public:
-        CRefCounter() { i_count; }
+        CRefCounter() { i_count = 0; }
         int iAdd() { return(++i_count); }
         int iDec() { return(--i_count); };
+        int get()  { return i_count; }
     private:
         int i_count;
 };
@@ -14,40 +19,51 @@ class CMySmartPointer
     public:
         CMySmartPointer(T *pcPointer)
         {
+            cout << "Initailized smart pointer: " << pcPointer << endl;
             pc_pointer = pcPointer;
             pc_counter = new CRefCounter();
             pc_counter->iAdd();
         }
+
         CMySmartPointer(const CMySmartPointer &pcOther)
         {
             pc_pointer = pcOther.pc_pointer;
             pc_counter = pcOther.pc_counter;
             pc_counter->iAdd();
+        
+            cout << "Copied smart pointer: " << pc_pointer << ". Number of copies: " << pc_counter->get() << endl;
         }
+
         ~CMySmartPointer()
         {
             if (pc_counter->iDec() == 0)
             {
-                std::cout << "Deleting " << pc_pointer << std::endl;
                 delete pc_pointer;
                 delete pc_counter;
+                cout << "Deleted " << pc_pointer << endl;
             }
         }
+
         T& operator*() { return(*pc_pointer); }
         T* operator->() { return(pc_pointer); }
-        T& operator=(const T& other) {
-            if (other != pc_pointer) {
+        CMySmartPointer<T>& operator=(const CMySmartPointer<T> &other) {
+            if (other.pc_pointer != pc_pointer) {
                 if (pc_counter->iDec() == 0) {
                     delete pc_pointer;
                     delete pc_counter;
+                    cout << "Deleted " << pc_pointer << endl;
                 }
 
-                pc_counter = new CRefCounter();
+                pc_counter = other.pc_counter;
+                pc_pointer = other.pc_pointer;
+                cout << "New pointer assigned: " << pc_pointer << ". Number of copies: " << pc_counter->get() << endl;
+            } else {
+                delete pc_counter;
+                pc_counter = other.pc_counter;
                 pc_counter->iAdd();
-                pc_pointer = other;
             }
 
-            return this;
+            return *this;
         }
 
     private:
--- semester-3/tep/lab7/main.cpp
+++ semester-3/tep/lab7/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "CMySmartPointer.h"
 
 using namespace std;
@@ -7,14 +8,46 @@ int main() {
     {
         string* str1 = new string("String 1");
         string* str2 = new string("String 2");
-        cout << "Initializing smart pointer for dynamically allocated string" << endl;
+        string* str3 = new string("String 3");
+
+        cout << "\n----------------------------------------------------------------" << endl;
+        cout << "Initializing smart pointers for 3 dynamically allocated strings" << endl;
+        cout << "----------------------------------------------------------------" << endl;
         CMySmartPointer<string> strPointer1(str1);
         CMySmartPointer<string> strPointer2(str2);
+        CMySmartPointer<string> strPointer3(str3);
+
+        cout << "\n\n-------------------------------" << endl;
+        cout << "Copying the first smart pointer" << endl;
+        cout << "-------------------------------" << endl;
+        CMySmartPointer<string> strPointer1Copy(strPointer1);
+
+        cout << "\n\n--------------------------------------" << endl;
+        cout << "Assigning first smart pointer to third" << endl;
+        cout << "--------------------------------------" << endl;
+        strPointer3 = strPointer1;
+
+        cout << "\n\n-------------------------------------------------------------------------" << endl;
+        cout << "Assigning first smart pointer to another, which points to the same string" << endl;
+        cout << "-------------------------------------------------------------------------" << endl;
+        CMySmartPointer<string> strPointer4(str1);
+        strPointer4 = strPointer1;
+
+        cout << "\n\n------------" << endl;
+        cout << "End of block" << endl;
+        cout << "------------" << endl;
     }
 
     {
-        string str("Hello there");
-        CMySmartPointer<string> strPointer(&str);
-    }
+        int num = 1;
+        cout << "\n\n\n---------------------------------------------------------" << endl;
+        cout << "Initializing a smart pointer for statically allocated int" << endl;
+        cout << "---------------------------------------------------------" << endl;
 
+        CMySmartPointer<int> intPointer(&num);
+
+        cout << "\n\n------------" << endl;
+        cout << "End of block" << endl;
+        cout << "------------" << endl;
+    }
 }
