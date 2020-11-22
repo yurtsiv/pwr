248c2717c62128269698538d3c2759c873705136
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2020-11-22 16:16:33 +0100

    scripting langs lab 7 done

---
+++ semester-5/scripting-langs/lab7/const.py
@@ -0,0 +1,10 @@
+import os
+
+DATE_COLUMN = 0
+CASES_COLUMN = 4
+DEATHS_COLUMN = 5
+COUNTRY_NAME_COLUMN = 6
+COUNTRY_CODE_COLUMN = 8
+CONTINENT_COLUMN = 10
+FILE_PATH = os.path.dirname(__file__) + "/Covid.txt"
+DATE_FORMAT = "%d/%m/%Y"
---
+++ semester-5/scripting-langs/lab7/main.py
@@ -0,0 +1,86 @@
+from Cases_world import Cases_world
+from Cases_country import Cases_country
+from Cases_day import Cases_day
+from Country_names import Country_names
+from const import COUNTRY_NAME_COLUMN, COUNTRY_CODE_COLUMN, CONTINENT_COLUMN, FILE_PATH, DATE_FORMAT
+from utils import flatten_list
+from itertools import groupby
+
+cases_world = Cases_world()
+country_names = Country_names()
+
+# Populate cases_world & country_names
+with open(FILE_PATH, 'r') as csv_file:
+  csv_file.readline() # skip header
+
+  for row in csv_file.readlines():
+    cases_day = Cases_day.from_row(row)
+    columns = row.split('\t')
+    country_name = columns[COUNTRY_NAME_COLUMN]
+    country_code = columns[COUNTRY_CODE_COLUMN]
+    continent = columns[CONTINENT_COLUMN]
+    cases_country = cases_world.get_cases_country(country_code)
+
+    country_names.add_country(country_code, country_name, continent)
+
+    if cases_country is None:
+      cases_world.add_cases_country(
+        Cases_country(country_code, [cases_day])
+      )
+    else:
+      cases_country.add_cases_day(cases_day)
+
+cases_countries = cases_world.get_countries()
+
+# Most deaths
+most_deaths_country = max(cases_countries, key=lambda c: c.total_deaths)
+most_deaths_country_name = country_names.get_name_by_code(most_deaths_country.country_code)
+print("Most deaths: " + most_deaths_country_name)
+
+# Most cases
+most_cases_country = max(cases_countries)
+most_cases_country_name = country_names.get_name_by_code(most_cases_country.country_code)
+print("\nMost cases: " + most_cases_country_name)
+
+# Worst day in the world
+all_days = flatten_list(
+  map(lambda country: country.all_days, cases_countries)
+)
+
+all_days.sort(key=lambda day: day.day)
+worst_day_date = all_days[0].day
+worst_day_deaths = all_days[0].deaths
+
+for day, day_cases in groupby(all_days, key=lambda day: day.day):
+  deaths = sum(map(lambda day: day.deaths, list(day_cases)))
+
+  if deaths > worst_day_deaths:
+    worst_day_date = day
+    worst_day_deaths = deaths
+
+print("\nWorst day in the world: %s  %d deaths" % (worst_day_date.strftime(DATE_FORMAT), worst_day_deaths))
+
+# Worst day for each continent
+print("\n-- Worst days for each continent --\n")
+countries_in_continents = country_names.countries_in_continents
+for continent in countries_in_continents.keys():
+  cases_continent_countries = map(
+    lambda code: cases_world.get_cases_country(code),
+    countries_in_continents[continent]
+  )
+
+  continent_days = flatten_list(
+    map(lambda country: country.all_days, cases_continent_countries)
+  )
+
+  worst_day = max(continent_days, key=lambda day: day.deaths)
+
+  print("Worst day for " + continent + ": " + str(worst_day))
+
+# Worst day for each country
+print("\n-- Worst days for each country --\n")
+for cases_country in cases_countries:
+  worst_day = max(cases_country.all_days, key=lambda day: day.deaths)
+  country_name = country_names.get_name_by_code(cases_country.country_code)
+
+  print("Worst day for " + country_name + ": " + str(worst_day)) 
