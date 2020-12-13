c0e88f5b5ecdf7db41a1394d857c5c7b85e05c97
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2020-12-13 15:23:39 +0100

    oracle lab3 partial

--- semester-5/oracle/Lab/Lab3.sql
+++ semester-5/oracle/Lab/Lab3.sql
@@ -1,7 +1,6 @@
 SET SERVEROUTPUT ON
 
--- Zad 34
-
+-- Zadanie 34
 DECLARE
     var_funkcja Kocury.funkcja%TYPE := '&Funkcja';
     kocury_num NUMBER;
@@ -15,8 +14,7 @@ BEGIN
     END IF;
 END;
 
--- Zad 35
-
+-- Zadanie 35
 DECLARE
     var_pseudo Kocury.pseudo%TYPE := '&Pseudo';
     kocur Kocury%ROWTYPE;
@@ -44,4 +42,116 @@ BEGIN
 EXCEPTION
     WHEN no_data_found
     THEN DBMS_OUTPUT.PUT_LINE('Nie znaleziono kota o podanym pseudo');
+END;
+
+-- Zadanie 36
+DECLARE
+    CURSOR do_podwyzki IS
+        SELECT pseudo, przydzial_myszy, max_myszy
+        FROM Kocury NATURAL JOIN Funkcje
+        ORDER BY przydzial_myszy
+        FOR UPDATE OF przydzial_myszy;
+
+    sum_myszy NUMBER;
+    next_myszy NUMBER;
+    sum_updates NUMBER := 0;
+BEGIN
+    SELECT SUM(przydzial_myszy) INTO sum_myszy FROM Kocury;
+
+    <<loop1>>LOOP
+        FOR re IN do_podwyzki
+        LOOP
+            next_myszy := LEAST(re.max_myszy, ROUND(1.1 * re.przydzial_myszy));
+
+            UPDATE Kocury
+            SET przydzial_myszy = next_myszy
+            WHERE pseudo = re.pseudo;
+            
+            sum_myszy := sum_myszy + (next_myszy - re.przydzial_myszy);
+            
+            IF next_myszy <> re.przydzial_myszy THEN
+                sum_updates := sum_updates + 1;
+            END IF;
+
+            EXIT loop1 WHEN sum_myszy > 1050;
+        END LOOP;
+    END LOOP loop1;
+
+    DBMS_OUTPUT.PUT_LINE('Calk. przydzial w stadku ' || sum_myszy || ' Zmian - ' || sum_updates);
+END;
+
+SELECT imie, przydzial_myszy "Myszki po podwyzce"
+FROM Kocury
+ORDER BY przydzial_myszy;
+
+ROLLBACK;
+
+-- Zadanie 37
+DECLARE
+    CURSOR koty IS
+        SELECT (przydzial_myszy + NVL(myszy_extra, 0)) zjada, pseudo
+        FROM Kocury
+        ORDER BY zjada DESC;
+
+    nr NUMBER := 1;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Nr    Pseudonim    Zjada');
+    DBMS_OUTPUT.PUT_LINE('------------------------');
+
+    FOR kot IN koty
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(LPAD(nr, 2) || ' ' || LPAD(kot.pseudo, 12)|| ' ' || LPAD(kot.zjada, 8));
+        nr := nr + 1;
+        EXIT WHEN nr > 5;
+    END LOOP;
+END;
+
+
+-- Zadanie 38
+DECLARE
+    max_depth POSITIVE := 3; -- '&Maks';
+    var_depth POSITIVE;
+
+    CURSOR koty IS
+        SELECT imie, szef
+        FROM Kocury
+        WHERE funkcja IN ('KOT', 'MILUSIA');
+    
+    kot_tmp koty%ROWTYPE;
+BEGIN
+    SELECT MAX(LEVEL) - 1 INTO var_depth
+    FROM Kocury
+    CONNECT BY szef = PRIOR pseudo
+    START WITH szef IS NULL;
+    
+    var_depth := LEAST(var_depth, max_depth);
+
+    -- Header
+    DBMS_OUTPUT.PUT('| ' || RPAD('Imie', 15));
+    FOR i IN 1..var_depth
+    LOOP
+        DBMS_OUTPUT.PUT( '| ' || RPAD('Szef ' || i, 15));
+    END LOOP;
+    DBMS_OUTPUT.NEW_LINE();
+    DBMS_OUTPUT.PUT_LINE(RPAD('-', (var_depth + 1) * 15, '-'));
+    
+    FOR kot IN koty
+    LOOP
+        DBMS_OUTPUT.PUT( '| ' || RPAD(kot.imie, 15));
+
+        kot_tmp := kot;
+        FOR i IN 1..var_depth
+        LOOP
+            IF  kot_tmp.szef IS NULL THEN
+                DBMS_OUTPUT.PUT('| ' || RPAD(' ', 15));
+            ELSE
+                SELECT imie, szef INTO kot_tmp
+                FROM Kocury
+                WHERE pseudo = kot_tmp.szef;
+                DBMS_OUTPUT.PUT('| ' || RPAD(kot_tmp.imie, 15));
+            END IF;
+        END LOOP;
+
+        DBMS_OUTPUT.NEW_LINE();
+    END LOOP;
 END;
