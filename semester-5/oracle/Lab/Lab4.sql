95d6660c6d0ecffc737d5c262d5b16f0b5c7c52d
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-01-23 14:30:41 +0100

    oracle lab4 partial

--- semester-5/oracle/Lab/Lab4.sql
+++ semester-5/oracle/Lab/Lab4.sql
@@ -3,10 +3,6 @@ DROP TYPE PlebsO FORCE;
 DROP TYPE ElitaO FORCE;
 DROP TYPE KontoWpisO FORCE;
 
-DROP TYPE plebs FORCE;
-
--- DROP TABLE Bandy CASCADE CONSTRAINTS;
--- DROP TABLE Funkcje CASCADE CONSTRAINTS;
 DROP TABLE Elita CASCADE CONSTRAINTS; 
 DROP TABLE Plebs CASCADE CONSTRAINTS;
 DROP TABLE Kocury_t CASCADE CONSTRAINTS; 
@@ -14,20 +10,43 @@ DROP TABLE Kocury_t CASCADE CONSTRAINTS;
 CREATE OR REPLACE TYPE KotO AS OBJECT
 (
     imie VARCHAR2(15),
-    plec VARCHAR(1),
-    pseudo VARCHAR(15),
+    plec VARCHAR2(1),
+    pseudo VARCHAR2(15),
     funkcja VARCHAR2(10),
-    szef VARCHAR(15),
+    szef VARCHAR2(15),
     w_stadku_od DATE,
     przydzial_myszy NUMBER(3),
-    myszy_extra NUMBER(3)
+    myszy_extra NUMBER(3),
+
+    MEMBER FUNCTION calk_przydzial RETURN NUMBER
 );
+/
+CREATE OR REPLACE TYPE BODY KotO AS
+    MEMBER FUNCTION calk_przydzial RETURN NUMBER IS
+    BEGIN
+        RETURN przydzial_myszy + NVL(myszy_extra, 0);
+    END;
+END;
+/
 
 CREATE OR REPLACE TYPE PlebsO AS OBJECT
 (
     idn NUMBER(3),
-    kot REF KotO
+    kot REF KotO,
+    
+    MEMBER FUNCTION pseudo RETURN VARCHAR2
 );
+/
+
+CREATE OR REPLACE TYPE BODY PlebsO AS
+    MEMBER FUNCTION pseudo RETURN VARCHAR2 IS
+        ps VARCHAR(15);
+    BEGIN
+        SELECT DEREF(kot).pseudo INTO ps FROM dual;
+        RETURN ps;
+    END;
+END;
+/
 
 CREATE OR REPLACE TYPE ElitaO AS OBJECT
 (
@@ -35,7 +54,7 @@ CREATE OR REPLACE TYPE ElitaO AS OBJECT
     kot REF KotO,
     sluga REF PlebsO
 );
-
+/
 
 CREATE OR REPLACE TYPE KontoWpisO AS OBJECT
 (
@@ -43,19 +62,22 @@ CREATE OR REPLACE TYPE KontoWpisO AS OBJECT
     dataUsuniecia DATE,
     wlasciciel REF ElitaO
 );
-
+/
 
 CREATE TABLE Kocury_t OF KotO (
     CONSTRAINT kocury_t_pk PRIMARY KEY (pseudo)
-)
+);
+/
 
 CREATE TABLE Plebs OF PlebsO (
     CONSTRAINT plebs_pk PRIMARY KEY (idn)
-)
+);
+/
 
 CREATE TABLE Elita OF ElitaO (
     CONSTRAINT elita_pk PRIMARY KEY (idn)
-)
+);
+/
 
 INSERT ALL
   INTO Kocury_t VALUES (KotO('JACEK','M','PLACEK','LOWCZY','LYSY','2008-12-01',67,NULL))
@@ -77,16 +99,14 @@ INSERT ALL
   INTO Kocury_t VALUES (KotO('KSAWERY','M','MAN','LAPACZ','RAFA','2008-07-12',51,NULL))
   INTO Kocury_t VALUES (KotO('MELA','D','DAMA','LAPACZ','RAFA','2008-11-01',51,NULL))
 SELECT * FROM dual;
-
+/
 
 DELETE FROM Plebs;
 INSERT INTO Plebs
     SELECT PlebsO(ROWNUM, REF(K))
     FROM Kocury_t K
-    WHERE K.funkcja NOT IN ('SZEFUNIO', 'BANDZIOR', 'MILUSIA')
-
-
-DELETE FROM Elita;
+    WHERE K.funkcja NOT IN ('SZEFUNIO', 'BANDZIOR', 'MILUSIA');
+/
 
 INSERT INTO Elita
     SELECT ElitaO(
@@ -95,8 +115,10 @@ INSERT INTO Elita
         NULL
     )
     FROM Kocury_t K
-    WHERE K.funkcja IN ('SZEFUNIO', 'BANDZIOR', 'MILUSIA')
+    WHERE K.funkcja IN ('SZEFUNIO', 'BANDZIOR', 'MILUSIA');
+/
 
+-- Przypisz losowych sług do elity
 DECLARE
     CURSOR elita_c IS SELECT idn FROM Elita;
     sluga_rand NUMBER := 0;
@@ -116,22 +138,16 @@ BEGIN
         WHERE idn = e.idn;
     END LOOP;
 END;
+/
 
-UPDATE Elita
-SET sluga = (
-    SELECT * FROM
-    (SELECT REF(P) FROM Plebs P
-        ORDER BY dbms_random.value
-    )
-    WHERE rownum = 1
-)
+SELECT idn, P.pseudo()
+FROM Plebs P;
+/
 
-SELECT * FROM
-( SELECT idn FROM Plebs
-ORDER BY dbms_random.value )
-WHERE rownum = 1
-
-SELECT idn FROM Plebs T WHERE ROWNUM = TRUNC(DBMS_RANDOM.value(0, 3))
+SELECT pseudo, K.calk_przydzial()
+FROM Kocury_t K;
+/
 
 SELECT idn, VALUE(E).kot.imie, VALUE(E).sluga.kot.imie
 FROM Elita E;
+/
