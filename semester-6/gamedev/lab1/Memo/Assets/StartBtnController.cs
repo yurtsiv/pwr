9dff68a927efbf0b74f51df5af66ed762a538836
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-03-10 22:46:22 +0100

    gamedev lab1 init

---
+++ semester-6/gamedev/lab1/Memo/Assets/StartBtnController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StartBtnController : MonoBehaviour, IPointerClickHandler
+{
+  // Start is called before the first frame update
+  public Button btn;
+
+  public void OnPointerClick(PointerEventData d)
+  {
+    Debug.Log("HORRAY");
+  }
+
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
