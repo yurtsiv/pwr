15120fb21a5cda0582df49b61ef6e70676379a5a
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-03-16 23:53:17 +0100

    rsi lab 1 part 2

---
+++ semester-6/rsi/lab1/part2/xmlrpcclient/src/ClientRPC.java
@@ -0,0 +1,61 @@
+import org.apache.xmlrpc.XmlRpcClient;
+
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.Collectors;
+
+public class ClientRPC {
+    private static String SERVER_NAME = "MyServer";
+
+    static void mainMenu(Vector<Vector<String>> methods, XmlRpcClient srv) {
+        AC callback = new AC();
+
+        while (true) {
+            List<String> options = methods.stream().map(m -> m.get(0) + " " + m.get(1)).collect(Collectors.toList());
+
+            int option = UserInteraction.selectOption("Choose method", options);
+
+            String[] paramsTypes = methods.get(option).get(2).split(",");
+            Vector<Object> params = new Vector<>();
+
+            for (String type : paramsTypes) {
+                if (type.equals("int")) {
+                    params.addElement(UserInteraction.getInt("Enter an int", Integer.MIN_VALUE, Integer.MAX_VALUE));
+                } else if (type.equals("String")) {
+                    params.addElement(UserInteraction.getStr("Enter a string"));
+                } else {
+                    throw new IllegalArgumentException("Unsupported param type");
+                }
+            }
+
+            boolean asyncMethod = methods.get(option).get(4).equals("async");
+            String methodName = SERVER_NAME + "." + methods.get(option).get(0);
+
+            try {
+                if (asyncMethod) {
+                    srv.executeAsync(methodName, params, callback);
+                } else {
+                   Object result = srv.execute(methodName, params);
+                   System.out.println("Result: " + result);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+//            Scanner sc = new Scanner(System.in);
+//            System.out.println("Server url: ");
+
+//            String url = sc.nextLine();
+            XmlRpcClient srv = new XmlRpcClient("http://localhost:3000");
+
+            Vector<Vector<String>> methods = (Vector<Vector<String>>) srv.execute(SERVER_NAME + ".show", new Vector<>());
+            mainMenu(methods, srv);
+        } catch (Exception e) {
+            System.err.println("XML-RPC client: " + e);
+        }
+    }
+}
