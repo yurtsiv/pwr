bab5e1a614aa375a3a0bb81249dfe51e1593c25f
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-04-08 00:22:02 +0200

    gamedev: open/close settings

--- semester-6/gamedev/symmetrical-octo-enigma/Assets/Scripts/Player.cs
+++ semester-6/gamedev/symmetrical-octo-enigma/Assets/Scripts/Player.cs
@@ -1,10 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public int gems = 0;
+    public GameObject settingsPanel;
 
-    public void ReceiveGem() {
+    public void ReceiveGem()
+    {
         gems++;
+    }
+
+    public void ToggleSettingsPanel(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            settingsPanel.SetActive(!settingsPanel.active);    
+        }
     }
 }
