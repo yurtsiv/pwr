0bec7ccfffb56901bea2195c2766288238c2823c
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-04-07 21:55:33 +0200

    gamedev: moving platform

---
+++ semester-6/gamedev/symmetrical-octo-enigma/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed = 10;
+    public Rigidbody2D body;
+
+    float initialX;
+    Vector2 velocity;
+
+    void Start()
+    {
+        initialX = transform.position.x;
+        velocity = new Vector2(speed, 0);
+    }
+
+    void Update()
+    {
+        velocity.x = speed * Mathf.Sign(velocity.x);
+
+        body.velocity = velocity;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "PlatformStop") {
+            velocity.x = -velocity.x;
+        }
+    }
+}
