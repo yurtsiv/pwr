4d036b0d6efa424d4e694ad903c7eb83f618292d
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2021-04-08 12:26:01 +0200

    gamedev: level 2

--- semester-6/gamedev/symmetrical-octo-enigma/Assets/Scripts/Player.cs
+++ semester-6/gamedev/symmetrical-octo-enigma/Assets/Scripts/Player.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int gems = 0;
     public GameObject settingsPanel;
     public Transform respawnPoint;
+
+    int numOfScenes = 2;
+    int currentScene = 0;
 
     void Start()
     {
@@ -39,7 +43,9 @@ public class Player : MonoBehaviour
 
         if (hitInfo.gameObject.tag == "Finish")
         {
+            currentScene = (currentScene + 1) % (numOfScenes + 1);
 
+            SceneManager.LoadScene($"Level{currentScene}", LoadSceneMode.Single);
         }
     }
 }
