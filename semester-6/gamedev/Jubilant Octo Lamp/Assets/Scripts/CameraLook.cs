c0dce700c03d31ab9f03f22b56b3600f9c445d54
Stepan <yurtsiv.stepan@gmail.com>
2021-05-11 11:43:11 +0200

    gamedev lab4 ground & skybox

--- semester-6/gamedev/Jubilant Octo Lamp/Assets/Scripts/CameraLook.cs
+++ semester-6/gamedev/Jubilant Octo Lamp/Assets/Scripts/CameraLook.cs
@@ -20,7 +20,7 @@ public class CameraLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
+        xRotation += mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
