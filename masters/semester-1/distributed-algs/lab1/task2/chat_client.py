a6b681e2f574931b91c1c81fcda6b9c4b808e9e7
Stepan Yurtsiv <yurtsiv.stepan@gmail.com>
2022-04-02 00:45:08 +0200

    dist. alg. playing around with ncurses

--- masters/semester-1/distributed-algs/lab1/task2/chat_client.py
+++ masters/semester-1/distributed-algs/lab1/task2/chat_client.py
@@ -1,16 +1,76 @@
 from email import message
-from unicodedata import name
+import sys
+from tkinter import CURRENT
+from typing import Text
+
+from matplotlib.pyplot import text
+sys.path.append("proto")
+
+import threading
+import curses
+from curses.textpad import Textbox, rectangle
+from curses import wrapper
 import grpc
 import proto.chat_pb2_grpc as pb2_grpc
 import proto.chat_pb2 as proto
 
 
-channel = grpc.insecure_channel('localhost:50051')
-stub = pb2_grpc.ChatServerStub(channel)
+def connect():
+    channel = grpc.insecure_channel('localhost:50051')
+    stub = pb2_grpc.ChatServerStub(channel)
+    return stub
 
-stub.SendNote(
-    proto.Note(
-        name="Stepan",
-        message="Hello"
-    )
-)
+def main(stdscr):
+    stdscr.clear()
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    USERNAME_COLOR = curses.color_pair(1)
+    stdscr.nodelay(True)
+
+    stdscr.addstr("Connecting to the server...")
+    stub = connect()
+    stdscr.clear()
+    stdscr.refresh()
+
+    chat_pad = curses.newwin(curses.LINES - 2, curses.COLS, 0, 0)
+    msg_input_win = curses.newwin(1, curses.COLS, curses.LINES - 1, 0)
+    textbox = Textbox(msg_input_win)
+
+    def redraw(messages):
+        chat_pad.clear()
+        chat_pad.refresh()
+
+        for m in messages:
+            chat_pad.addstr("\n" + m.name, USERNAME_COLOR)
+            chat_pad.addstr(": " + m.message)
+
+        # chat_pad.refresh(0, 0, 0, 0, curses.LINES - 2, curses.COLS)
+        chat_pad.refresh()
+
+    def receive_messages():
+        messages = []
+        for message in stub.ChatStream(proto.Empty()):
+            messages.append(message)
+            redraw(messages)
+
+    def listen_textbox():
+        while True:
+            textbox.edit()
+            text = textbox.gather()
+            stub.SendNote(
+                proto.Note(
+                    name="Test",
+                    message=text
+                )
+            )
+            msg_input_win.clear()
+            msg_input_win.refresh()
+
+    redraw([])
+    r = threading.Thread(target=receive_messages)
+    l = threading.Thread(target=listen_textbox)
+    r.start()
+    l.start()
+    l.join()
+    r.join()
+
+wrapper(main)
--- masters/semester-1/distributed-algs/lab1/task2/chat_server.py
+++ masters/semester-1/distributed-algs/lab1/task2/chat_server.py
@@ -1,27 +1,36 @@
+from email import message
+import sys
+sys.path.append("proto")
+
 from concurrent import futures
 import grpc
 import proto.chat_pb2_grpc as pb2_grpc
+import proto.chat_pb2 as proto
 
 class ChatServicer(pb2_grpc.ChatServerServicer):
     def __init__(self):
-        self.messages = []
+        self.messages = [
+            proto.Note(name="server", message="server")
+        ]
     
     def ChatStream(self, request, context):
-        received_count = len(self.messages) - 1
+        received_count = 0
         while True:
             while received_count < len(self.messages):
-                yield self.messages[received_count]
+                yield self.messages[received_count - 1]
                 received_count += 1
 
     def SendNote(self, request, context):
-        print(request)
-
+        print("Received", request)
+        self.messages.append(request)
+        return proto.Empty()
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     pb2_grpc.add_ChatServerServicer_to_server(ChatServicer(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
+    print("Server started. Press Ctrl+C to stop")
     server.wait_for_termination()
 
 serve()
